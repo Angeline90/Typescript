db9e6e692f72c34adf559d0f3fcdfd0f62db2e57
Angeline90 <angeline.gauron@gmail.com>
2024-04-09 10:06:32 +0200

    POO quest

---
+++ Person.ts
@@ -0,0 +1,28 @@
+class Person {
+  constructor(private name: string, private age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  getName(): string {
+    return this.name;
+  }
+
+  getAge(): number {
+    return this.age;
+  }
+
+  tellMyName():string {
+    return `I am ${this.name}`;
+  }
+
+  tellMyAge():string {
+    return `I am ${this.age} years old`;
+  }
+}
+
+const person = new Person("John", 40);
+const person1 = new Person("Mary", 35);
+
+console.log(person.tellMyName(), person.tellMyAge());
+console.log(person1.tellMyName(), person1.tellMyAge());
---
+++ build/Person.js
@@ -0,0 +1,25 @@
+"use strict";
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+        this.name = name;
+        this.age = age;
+    }
+    getName() {
+        return this.name;
+    }
+    getAge() {
+        return this.age;
+    }
+    tellMyName() {
+        return `I am ${this.name}`;
+    }
+    tellMyAge() {
+        return `I am ${this.age} years old`;
+    }
+}
+const person = new Person("John", 40);
+const person1 = new Person("Mary", 35);
+console.log(person.tellMyName(), person.tellMyAge());
+console.log(person1.tellMyName(), person1.tellMyAge());
